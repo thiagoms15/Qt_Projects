d14e1c41b0935de776e14b36bba203d16f1277e1
Thiago Marques <thiagoms.15@gmail.com>
2017-12-02 15:45:44 -0200

    Revomed build files to vesrion

--- primeiro_aplicativo/build-primeiroApp-Android_for_armeabi_v7a_GCC_4_9_Qt_5_5_1-Debug/android-build/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/thiago/Documents/Qt/primeiro_aplicativo/build-primeiroApp-Android_for_armeabi_v7a_GCC_4_9_Qt_5_5_1-Debug/android-build/bin/AndroidManifest.xml : \
-/home/thiago/Documents/Qt/primeiro_aplicativo/build-primeiroApp-Android_for_armeabi_v7a_GCC_4_9_Qt_5_5_1-Debug/android-build/AndroidManifest.xml \
--- primeiro_aplicativo/build-primeiroApp-Android_for_armeabi_v7a_GCC_4_9_Qt_5_5_1-Debug/android-build/gen/org/kde/necessitas/ministro/IMinistro.java.d
+++
@@ -1,5 +0,0 @@
-/home/thiago/Documents/Qt/primeiro_aplicativo/build-primeiroApp-Android_for_armeabi_v7a_GCC_4_9_Qt_5_5_1-Debug/android-build/gen/org/kde/necessitas/ministro/IMinistro.java: \
-  /home/thiago/Documents/Qt/primeiro_aplicativo/build-primeiroApp-Android_for_armeabi_v7a_GCC_4_9_Qt_5_5_1-Debug/android-build/src/org/kde/necessitas/ministro/IMinistro.aidl \
-  /home/thiago/Documents/Qt/primeiro_aplicativo/build-primeiroApp-Android_for_armeabi_v7a_GCC_4_9_Qt_5_5_1-Debug/android-build/src/org/kde/necessitas/ministro/IMinistroCallback.aidl 
-
-/home/thiago/Documents/Qt/primeiro_aplicativo/build-primeiroApp-Android_for_armeabi_v7a_GCC_4_9_Qt_5_5_1-Debug/android-build/src/org/kde/necessitas/ministro/IMinistroCallback.aidl :
